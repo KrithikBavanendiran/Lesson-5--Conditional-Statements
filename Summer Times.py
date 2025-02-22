af5d35cafbc4c6b5b5301787cb4286703ee82ea3
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-22 17:44:38 +0000

    Summer Times

---
+++ Summer Times.py
@@ -0,0 +1,12 @@
+temp=int(input("What temperature is it outside? "))
+if(temp<16):
+    print("Don't go outside")
+else:
+    print("Enjoy your day")
+
+wind=input("Is it windy? Yes or No: ")
+if(wind=="No"):
+    print("Don't go outside")
+else:
+    print("Enjoy your day")
+
